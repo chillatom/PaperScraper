3e0dc578fb85308771ad34de5e5ab5a743785917
Corey Hill <coreyhill314@gmail.com>
2018-04-16 14:13:00 -0400

    Continued setup. - initial file creation - email template instantiation

---
+++ PaperScraper/integrations.py
@@ -0,0 +1,9 @@
+# Integration Module
+#
+# Purpose: Provide functions for retrieving data from academic blogs / online journals.
+#          These functions only retrieve data and will not handle loading to disk/s3.
+#
+# Author: Corey Hill
+# Status: Dev
+# Version: 0.1.0
+
---
+++ PaperScraper/scraperController.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#
+# Scraper Controller Module
+#
+# Purpose: Main executable to be run.  Calls relevant modules to do the following
+#          1. Get scraped data
+#          2. Place scraped data into correct file store location
+#          3. Retrieve data for email
+#          4. Construct email from template
+#          5. Send email to distribution list
+#
+# Author: Corey Hill
+# Status: Dev
+# Version: 0.1.0
